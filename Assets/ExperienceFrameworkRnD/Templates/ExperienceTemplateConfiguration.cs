8d7a1390df6bc7081e6b1063a912240aee39e04f
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-05-11 14:04:31 +0100

    Refactor

--- Assets/ExperienceFrameworkRnD/Templates/ExperienceTemplateConfiguration.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace PhenomenalViborg.MUCOSDK
-{
-    [CreateAssetMenu(fileName = "NewExperienceTemplateConfiguration", menuName = "MUCOSDK/Experience Template Configuration")]
-    public class ExperienceTemplateConfiguration : ExperienceConfiguration
-    {
-    }
-}
